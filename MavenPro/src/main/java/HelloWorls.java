57d16abfd758268f877df9f9e750511e5d990145
maheshwar8622 <maheshwar8622@gmail.com>
2016-10-07 11:44:31 -0500

    Update HelloWorls.java

--- MavenPro/src/main/java/HelloWorls.java
+++ MavenPro/src/main/java/HelloWorls.java
@@ -4,6 +4,8 @@ public class HelloWorls {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("This is from Git remote");
 	}
 
 }
